6184f212f3c6c651c4a1a9e8bbde64063cdf8917
alisson-moura <alisson.mo.moura@outlook.com.br>
2022-06-27 10:08:21 -0300

    Alterando textos do banner

--- js/principal.js
+++ js/principal.js
@@ -1,8 +1,8 @@
-let banners = ['Os melhores do Brasil', 'Qualidade e preço baixo']
+let banners = ['Do lixo ao luxo!', 'Reaproveitaar é aproveitar!']
 let bannerAtual = 0
 
 function trocaBanner() {
-	bannerAtual = (bannerAtual + 1) % 2i
+	bannerAtual = (bannerAtual + 1) % 2
 	document.querySelector('h2#mensagem').textContent = banners[bannerAtual]
 }
 
